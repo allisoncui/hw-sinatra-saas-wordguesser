34a3b2c5d4ec7cf10ebb533ff59cad7dc30dc34d
ac5187 Allison Cui <ac5187@columbia.edu>
2025-09-22 06:04:45 +0000

    part 3

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -4,9 +4,38 @@ class WordGuesserGame
 
   # Get a word from remote "random word" service
 
+  attr_accessor :word, :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
+
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter == ""
+    ch = letter.downcase
+    raise ArgumentError unless ch.match?(/\A[a-z]\z/)
+    return false if @guesses.include?(ch) || @wrong_guesses.include?(ch)
+
+    if @word.downcase.include?(ch)
+      @guesses << ch
+    else
+      @wrong_guesses << ch
+    end
+    true
+  end
+
+  def word_with_guesses
+    @word.chars.map { |c| @guesses.include?(c.downcase) ? c : "-" }.join
+  end
+
+  def check_win_or_lose
+    return :win  if word_with_guesses == @word
+    return :lose if @wrong_guesses.length >= 7
+    :play
+  end
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
